8f828983086ac8e00e971abb0d29c844e3980bdf
Baorii3 <baorii310@gmail.com>
2024-03-16 00:32:57 +0100

    holaa

--- PruebasSemanitas/program.py
+++ PruebasSemanitas/program.py
@@ -1,15 +1,28 @@
+
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
+import pyautogui
 
 class Game():
     def __init__(self):
         self.menu = False
         self.gameStarted = False
+        self.time_paused = False
 
-    def hudmenu(self):
-        if self.gameStarted:
-           pass 
+    def hudmenu(self, key):
+        if key == "escape":
+            self.time_paused = not self.time_paused
+            application.paused = self.time_paused
+     
+    def fullscreen(self):
+        screen_width, screen_height = pyautogui.size()
+
+        window_x = (screen_width - screen_width) // 2
+        window_y = (screen_height - screen_height) // 2
+
+        window.size = (screen_width, screen_height)
+        window.position = (window_x, window_y)
 
     def start_game(self):
         destroy(button)
@@ -17,16 +30,16 @@ class Game():
         random.seed(0)
         Entity.default_shader = lit_with_shadows_shader
         ground = Entity(model='plane', collider='box', scale=64, texture='grass', texture_scale=(4,4))
-        editor_camera = EditorCamera(enabled=False, ignore_paused=True)
         player = FirstPersonController(model='cube', z=-10, color=color.orange, origin_y=-.5, speed=8, collider='box')
         player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
         self.setupWeapons()
+        self.camera = EditorCamera(enabled=False, ignore_paused=True)
         self.gameStarted = True
 
     def setupWeapons(self):
-        self.pistol = Weapon("Pistol", 8, 'cube', camera,(.5,-.25,.25), (.3,.2,1), -.5, color.red,1) 
+        self.pistol = Weapon("Pistol", 8,30, 'cube', camera,(.5,-.25,.25), (.3,.2,1), -.5, color.red,1) 
         self.pistol.muzzle_flash = Entity(parent=self.pistol, z=1, world_scale=.5, model='quad', color=color.yellow, enabled=False,)
-        self._pistol = Weapon("Pistol", 8, 'cube', camera,(-.5,-.25,.25), (.3,.2,1), -.5, color.green,0.5) 
+        self._pistol = Weapon("Pistol", 20,60, 'cube', camera,(-.5,-.25,.25), (.3,.2,1), -.5, color.green,0.5) 
         self._pistol.muzzle_flash = Entity(parent=self._pistol, z=1, world_scale=.5, model='quad', color=color.yellow, enabled=False)
         self._player = Player(self.pistol)
 
@@ -37,15 +50,16 @@ class Game():
 class HUD():
     def __init__(self):
         self.ammoText = Text(parent= camera.ui, text=f'Ammo: Not Set', y=-0.3, origin=(0, 0), background=True)
-    def updateAmmo(self, newAmmo, newMaxAmmo):
-        self.ammoText.text = f'Ammo: {newAmmo}/{newMaxAmmo}'
+    def updateAmmo(self, newAmmo, Cargador):
+        self.ammoText.text = f'Ammo: {newAmmo}/{Cargador}'
 
 class Weapon(Entity):
-    def __init__(self, name, maxAmmo,model,parent,position,scale,origin_z,color,time_cooldown):
+    def __init__(self, name, maxAmmo,cargador,model,parent,position,scale,origin_z,color,time_cooldown):
         super().__init__()
         self.name = name
         self.maxAmmo = maxAmmo
         self.ammo = maxAmmo
+        self.cargador = cargador
         self.model = model
         self.parent = parent
         self.position = position
@@ -75,6 +89,7 @@ class Weapon(Entity):
             self.reloading = False
             return
         self.ammo += 1 
+        self.cargador -= 1
         print(self.ammo)
 
 
@@ -101,15 +116,14 @@ class Player(Entity):
             
 def input(key):
     if game.gameStarted:
-        if key == "r" and not _player.current_weapon.cooldown:
+        if key == "r" and not game._player.current_weapon.cooldown:
             game._player.current_weapon.reload() 
 
-        if key == 'q'and not _player.current_weapon.cooldown and not _player.current_weapon.reloading:
+        if key == 'q'and not game._player.current_weapon.cooldown and not game._player.current_weapon.reloading:
             game._player.switch_weapon()
 
     if key == "enter":
         game.start_game()
-        print("hola")
 
 def update():
     if game.gameStarted:
@@ -120,15 +134,19 @@ def update():
             game._player.current_weapon.cooldown = True
             invoke(setattr, game._player.current_weapon, 'cooldown', False, delay=1)
             invoke(game._player.current_weapon.makeReload, delay= 0.5)
-        hud.updateAmmo(game._player.current_weapon.ammo, game._player.current_weapon.maxAmmo)
-        #_player.current_weapon.display_text.text = f'Ammo: {_player.current_weapon.ammo}/{_player.current_weapon.maxAmmo}'
+        hud.updateAmmo(game._player.current_weapon.ammo, game._player.current_weapon.cargador)
+
 
 hud = HUD()
 
 app = Ursina()
 game = Game()
-button = Button(text="Zombies", color=color.red, scale=(0.6, 0.1), position=(-0.5, 0.1))
-button2 = Button(text="Quit", color=color.black, scale=(0.6, 0.1), position=(-0.5, -0.1))
+button = Button(text="Zombies", color=color.red, scale=(0.6, 0.1), position=(0, 0.1))
+button2 = Button(text="Exit", color=color.black, scale=(0.6, 0.1), position=(0, -0.1))
+
+pause_handler = Entity(ignore_paused=True, input=game.hudmenu)
+
+#game.fullscreen()
 
 button.on_click = game.start_game
 button2.on_click = application.quit
--- PruebasSemanitas/pruebas.py
+++ PruebasSemanitas/pruebas.py
@@ -2,95 +2,6 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
 
-
-class Weapon(Entity):
-    def __init__(self, name, maxAmmo,model,parent,position,scale,origin_z,color,time_cooldown):
-        super().__init__()
-        self.name = name
-        self.maxAmmo = maxAmmo
-        self.ammo = maxAmmo
-        self.model = model
-        self.parent = parent
-        self.position = position
-        self.scale = scale
-        self.origin_z = origin_z
-        self.color = color
-        self.reloading = False
-        self.cooldown = False
-        self.time_cooldown = time_cooldown
-        self.display_text = Text(parent= camera.ui, text=f'Ammo: {self.ammo}/{self.maxAmmo}', background=True)
-        
-
-    def shoot(self):
-        self.display_text.disable = False
-        self.cooldown = True
-        self.muzzle_flash.enabled = True   
-        self.ammo -= 1
-        invoke(self.muzzle_flash.disable, delay=.05)
-        invoke(setattr,self,'cooldown', False, delay=self.time_cooldown)
-        print(self.ammo)
-        if self.ammo == 0:
-            self.reload()
-
-    def reload(self):
-        if self.ammo < self.maxAmmo:
-            self.reloading = True
-
-    def makeReload(self):
-        if self.ammo>= self.maxAmmo:
-            self.reloading = False
-            return
-        self.ammo += 1 
-        print(self.ammo)
-
-
-class Player(Entity):
-    def __init__(self):
-        super().__init__()
-        self.weapons = []
-        self.current_weapon_index = 0
-        self.current_weapon = pistol
-    def switch_weapon(self):
-        self.current_weapon.visible_setter(False)
-        self.current_weapon_index = (self.current_weapon_index + 1) % len(self.weapons)
-        self.current_weapon = self.weapons[self.current_weapon_index]
-        self.current_weapon.visible_setter(True)
-
-
-    def añadir_arma(self, arma):
-        self.weapons.append(arma)
-    
-    def input(self,key):
-        if key == "r" and not self.current_weapon.cooldown:
-            self.current_weapon.reload() 
-
-        if key == 'q'and not _player.current_weapon.cooldown and not self.current_weapon.reloading:
-            self.switch_weapon()
-
-        if key == "escape":
-            print("hola")
-
-    def update(self):
-        if held_keys["left mouse"] and not self.current_weapon.cooldown and not self.current_weapon.reloading:
-            self.current_weapon.shoot()
-
-        if self.current_weapon.reloading and not self.current_weapon.cooldown:
-            self.current_weapon.cooldown = True
-            invoke(setattr, self.current_weapon, 'cooldown', False, delay=1)
-            invoke(self.current_weapon.makeReload, delay= 0.5)
-
-            self.current_weapon.display_text.text = f'Ammo: {self.current_weapon.ammo}/{self.current_weapon.maxAmmo}'
-
-pistol = Weapon("Pistol", 8, 'cube', camera,(.5,-.25,.25), (.3,.2,1), -.5, color.red,1) 
-pistol.muzzle_flash = Entity(parent=pistol, z=1, world_scale=.5, model='quad', color=color.yellow, enabled=False)
-_pistol = Weapon("Pistol", 8, 'cube', camera,(-.5,-.25,.25), (.3,.2,1), -.5, color.green,0.5) 
-_pistol.muzzle_flash = Entity(parent=_pistol, z=1, world_scale=.5, model='quad', color=color.yellow, enabled=False)
-_pistol.visible_setter(False)
-_player = Player()
-_player.añadir_arma(pistol)
-_player.añadir_arma(_pistol)
-
-
 app = Ursina()
 
 random.seed(0)
@@ -102,6 +13,95 @@ editor_camera = EditorCamera(enabled=False, ignore_paused=True)
 player = FirstPersonController(model='cube', z=-10, color=color.orange, origin_y=-.5, speed=8, collider='box')
 player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
 
+gun = Entity(model='cube', parent=camera, position=(.5,-.25,.25), scale=(.3,.2,1), origin_z=-.5, color=color.red, on_cooldown=False)
+gun.muzzle_flash = Entity(parent=gun, z=1, world_scale=.5, model='quad', color=color.yellow, enabled=False)
+
+shootables_parent = Entity()
+mouse.traverse_target = shootables_parent
+
+
+for i in range(16):
+    Entity(model='cube', origin_y=-.5, scale=2, texture='brick', texture_scale=(1,2),
+        x=random.uniform(-8,8),
+        z=random.uniform(-8,8) + 8,
+        collider='box',
+        scale_y = random.uniform(2,3),
+        color=color.hsv(0, 0, random.uniform(.9, 1))
+        )
+
+def update():
+    if held_keys['left mouse']:
+        shoot()
+
+def shoot():
+    if not gun.on_cooldown:
+        # print('shoot')
+        gun.on_cooldown = True
+        gun.muzzle_flash.enabled=True
+        from ursina.prefabs.ursfx import ursfx
+        ursfx([(0.0, 0.0), (0.1, 0.9), (0.15, 0.75), (0.3, 0.14), (0.6, 0.0)], volume=0.5, wave='noise', pitch=random.uniform(-13,-12), pitch_change=-12, speed=3.0)
+        invoke(gun.muzzle_flash.disable, delay=.05)
+        invoke(setattr, gun, 'on_cooldown', False, delay=.15)
+        if mouse.hovered_entity and hasattr(mouse.hovered_entity, 'hp'):
+            mouse.hovered_entity.hp -= 10
+            mouse.hovered_entity.blink(color.red)
+
+
+from ursina.prefabs.health_bar import HealthBar
+
+class Enemy(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(parent=shootables_parent, model='cube', scale_y=2, origin_y=-.5, color=color.light_gray, collider='box', **kwargs)
+        self.health_bar = Entity(parent=self, y=1.2, model='cube', color=color.red, world_scale=(1.5,.1,.1))
+        self.max_hp = 100
+        self.hp = self.max_hp
+
+    def update(self):
+        dist = distance_xz(player.position, self.position)
+        if dist > 40:
+            return
+
+        self.health_bar.alpha = max(0, self.health_bar.alpha - time.dt)
+
+
+        self.look_at_2d(player.position, 'y')
+        hit_info = raycast(self.world_position + Vec3(0,1,0), self.forward, 30, ignore=(self,))
+        # print(hit_info.entity)
+        if hit_info.entity == player:
+            if dist > 2:
+                self.position += self.forward * time.dt * 5
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @hp.setter
+    def hp(self, value):
+        self._hp = value
+        if value <= 0:
+            destroy(self)
+            return
+
+        self.health_bar.world_scale_x = self.hp / self.max_hp * 1.5
+        self.health_bar.alpha = 1
+
+# Enemy()
+enemies = [Enemy(x=x*4) for x in range(4)]
+
+
+def pause_input(key):
+    if key == 'tab':    # press tab to toggle edit/play mode
+        editor_camera.enabled = not editor_camera.enabled
+
+        player.visible_self = editor_camera.enabled
+        player.cursor.enabled = not editor_camera.enabled
+        gun.enabled = not editor_camera.enabled
+        mouse.locked = not editor_camera.enabled
+        editor_camera.position = player.position
+
+        application.paused = editor_camera.enabled
+
+pause_handler = Entity(ignore_paused=True, input=pause_input)
 
 
 sun = DirectionalLight()
